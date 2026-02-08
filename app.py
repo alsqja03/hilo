4770f2392f4ea8fe9d533f2c54918c4a5481a67a
alsqja03 <144881118+alsqja03@users.noreply.github.com>
2026-02-09 03:08:35 +0900

    Change page title from 'Evolution Style Hi-Lo' to 'Hi-Lo'

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import random
 
 # --- 1. 페이지 및 스타일 설정 ---
-st.set_page_config(page_title="Evolution Style Hi-Lo", layout="centered")
+st.set_page_config(page_title="Hi-Lo", layout="centered")
 
 # CSS 스타일 주입
 st.markdown("""
